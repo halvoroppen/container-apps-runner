a3dbf884d5b0c341d9f5dd4bb21609c20e83518a
halvoroppen <42218834+halvoroppen@users.noreply.github.com>
2025-01-09 22:42:46 +0100

    Add support for gh app auth

--- github-actions-runner/entrypoint.sh
+++ github-actions-runner/entrypoint.sh
@@ -1,11 +1,58 @@
-#!/bin/sh -l
+#!/usr/bin/env bash
 
-# Retrieve a short lived runner registration token using the PAT
-REGISTRATION_TOKEN="$(curl -X POST -fsSL \
+set -o pipefail
+
+now=$(date +%s)
+iat=$((${now} - 60)) # Issued 60 seconds in the past
+exp=$((${now} + 600)) # Expires 10 minutes in the future
+
+b64enc() { openssl base64 | tr -d '=' | tr '/+' '_-' | tr -d '\n'; }
+
+header_json='{
+    "typ":"JWT",
+    "alg":"RS256"
+}'
+header=$(echo -n "${header_json}" | b64enc)
+
+payload_json='{
+    "iat":'"${iat}"',
+    "exp":'"${exp}"',
+    "iss":'"${APP_ID}"'
+}'
+payload=$(echo -n "${payload_json}" | b64enc)
+
+header_payload="${header}.${payload}"
+signature=$(openssl dgst -sha256 -sign <(echo -n "${PEM}") <(echo -n "${header_payload}") | b64enc)
+
+jwt="${header_payload}.${signature}"
+
+# Get an access token from the API using the installation ID of the GitHub app.
+access_token=$(curl -X POST -fsSL \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $jwt" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  "$ACCESS_TOKEN_API_URL" \
+  | jq -r '.token')
+
+# Retrieve a short lived runner registration token using the access token.
+registration_token=$(curl -X POST -fsSL \
   -H 'Accept: application/vnd.github.v3+json' \
-  -H "Authorization: Bearer $GITHUB_PAT" \
+  -H "Authorization: Bearer $access_token" \
   -H 'X-GitHub-Api-Version: 2022-11-28' \
   "$REGISTRATION_TOKEN_API_URL" \
-  | jq -r '.token')"
-# add --no-default-labels if needed
-./config.sh --url $GH_URL --token $REGISTRATION_TOKEN --unattended --ephemeral --labels $labels && ./run.sh
+  | jq -r '.token')
+
+# Register the ephemeral runner to run the job (--ephemeral ensures deregistration post job execution)
+# Use --disableupdate if you need to use the specific runner version contained in the docker image
+./config.sh --url $RUNNER_REGISTRATION_URL --token $registration_token --unattended --ephemeral --labels $labels && ./run.sh
+
+
+# # Retrieve a short lived runner registration token using the PAT
+# REGISTRATION_TOKEN="$(curl -X POST -fsSL \
+#   -H 'Accept: application/vnd.github.v3+json' \
+#   -H "Authorization: Bearer $GITHUB_PAT" \
+#   -H 'X-GitHub-Api-Version: 2022-11-28' \
+#   "$REGISTRATION_TOKEN_API_URL" \
+#   | jq -r '.token')"
+# # add --no-default-labels if needed
+# ./config.sh --url $GH_URL --token $REGISTRATION_TOKEN --unattended --ephemeral --labels $labels && ./run.sh
